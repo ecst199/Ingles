6533ae16516f3c07c377dbb6bf90ba8b410504e5
Steve <sjc.zambrano@yavirac.edu.ec>
2019-01-24 23:48:32 -0500

    oK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BodyComponent } from './body/body.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sidebar', component: SidebarComponent },
+  { path: 'body', component: BodyComponent },
+  { path: 'catalogue', component: CatalogueComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BodyComponent } from './body/body.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    BodyComponent,
+    CatalogueComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
